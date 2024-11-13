3e5e3f943417746e7462c57bbc87a5eefc044aba
Tenebris2 <buiducanh567@gmail.com>
2024-11-13 21:30:19 +0700

    added more for frontend and some api calling

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,20 @@
 import fastapi
 import pydantic
 import runner
+from fastapi.middleware.cors import CORSMiddleware
 
 app = fastapi.FastAPI()
+
+frontend_url = "http://localhost:3000"
+origins = [frontend_url]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Message(pydantic.BaseModel):
@@ -13,4 +25,10 @@ class Message(pydantic.BaseModel):
 async def read_root():
     # encrypted_message, rsa_obj = runner.rsa_runner(message)
     encrypted_message, rsa_obj = runner.rsa_cryptography("BUIDUCANH")
+    return {"message": str(hex(encrypted_message))}
+
+
+@app.post("/api/rsa/")
+async def rsa_cryptography_api(data: Message):
+    encrypted_message, rsa_obj = runner.rsa_cryptography(data.message)
     return {"message": str(hex(encrypted_message))}
